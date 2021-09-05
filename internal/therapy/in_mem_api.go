e8af2107eadcabc259926facb4a9e1baed67c978
serj <srdjan@adjust.com>
2021-09-05 19:01:59 +0200

    therapy api - rest of days added

--- internal/therapy/in_mem_api.go
+++ internal/therapy/in_mem_api.go
@@ -15,13 +15,20 @@ func NewInMemApi() *InMemApi {
 		Therapies: map[int]map[int]*Day{},
 	}
 
+	// hardcode therapy days for now
 	userId := 1
-	api.Therapies[userId] = map[int]*Day{}
-	api.Therapies[userId][1] = &Day{
-		Index:         1,
-		Title:         "Be calm and relax",
-		AudioFilePath: "/home/serj/anas-api-data/audio/user1/inu-user1-session1.mp3",
-		AudioURL:      fmt.Sprintf("/therapy/%d/%d/audio", userId, 1),
+	for i := 1; i <= 8; i++ {
+		title := "In progress..."
+		if i == 1 {
+			title = "Be calm and relax"
+		}
+		api.Therapies[userId] = map[int]*Day{}
+		api.Therapies[userId][i] = &Day{
+			Index:         i,
+			Title:         title,
+			AudioFilePath: fmt.Sprintf("/home/serj/anas-api-data/audio/user1/inu-user1-session%d.mp3", i),
+			AudioURL:      fmt.Sprintf("/therapy/%d/%d/audio", userId, i),
+		}
 	}
 
 	return api
